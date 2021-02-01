b53597b052867de2b6b0a0971cc0a694e63a49f8
Devin Sarao <devin.sarao@patelhs.org>
2021-02-01 18:52:37 +0000

    Commit day 1

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What is your legal name?");
+
+    String name = scan.next();
+
+    System.out.println("Hello " + name + " this automated bot will check your financial situation.");
+    System.out.println("What is your current salary?");
+
+    int currentSalary = scan.nextInt();
+   System.out.println("How much money did you earn/spend last year?");
+    
+
+    int salaryEarnedSpent = scan.nextInt();
+
+    int salary = getSalary(salaryEarnedSpent , currentSalary);
+
+    System.out.println("Welcome once again " + (name));
+    System.out.println("Your income is " + salary);
+    }
+
+    
+    
+
+ 
+
+
+
+   
+ 
+ 
 }
