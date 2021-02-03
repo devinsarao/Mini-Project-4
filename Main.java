e34871146e1b33161b8037adb4633447b2b890a1
Devin Sarao <devin.sarao@patelhs.org>
2021-02-03 17:37:42 +0000

    Added comments and debugged

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+//makes sure that scanner is working
 import java.util.Scanner;
-
+//public main class (no private needed in this project)
 class Main {
   public static void main(String[] args){
+    //scanner statement
     Scanner scan = new Scanner(System.in);
 
     System.out.println("What is your legal name?");
@@ -10,11 +12,11 @@ class Main {
 
     System.out.println("Hello " + name + " this automated bot will check your financial situation.");
     System.out.println("What is your current salary?");
-
+    //start of int statement
     int currentSalary = scan.nextInt();
    System.out.println("How much money did you earn/spend last year?");
     
-
+    //method calls
     int salaryEarnedSpent = scan.nextInt();
 
     int salary = getSalary(salaryEarnedSpent , currentSalary);
@@ -23,12 +25,13 @@ class Main {
     System.out.println("Your income is " + salary);
     }
 
-    
+    //retruns method calls
     static int getSalary(int salaryEarnedSpent , int currentsalary)
    {
     int salary = currentSalary + salaryEarnedSpent;
     return salary;
    }
+
 
  
 
